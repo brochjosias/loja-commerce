de0633597d7d28953aa88ef5703eb16e8008cde5
brochjosias <brochjosias@gmail.com>
2025-02-19 15:28:10 -0300

    filter-bar, config para rota api

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FilterBar } from "./components/filter-bar";
+import { ProductsList } from "./components/products-list";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const client = new QueryClient();
   return (
-    <div>
+    <QueryClientProvider client={client}>
       <main className={styles.main}>
         <FilterBar />
+        <ProductsList />
       </main>
-    </div>
+    </QueryClientProvider>
   );
 }
