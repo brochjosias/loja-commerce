c9bd96e692597cbd4bd2e251bfe562a8ed49c7d0
brochjosias <brochjosias@gmail.com>
2025-02-20 18:25:09 -0300

    Configurando página de detalhes do produto

--- front/src/types/product.ts
+++ front/src/types/product.ts
@@ -3,4 +3,13 @@ export interface Product {
   price_in_cents: number;
   id: string;
   image_url: string;
+  description?: string;
+  category?: string;
+}
+
+// Defina a estrutura esperada da resposta da API
+export interface ProductFetchResponse {
+  data: {
+    Product: Product;
+  };
 }
